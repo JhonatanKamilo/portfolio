510105ae4d0e229ff64b8064dc5fdc02acc3fd56
JhonatanKamilo <jhonatancamilo1535@gmail.com>
2026-03-23 02:43:04 -0500

    revert: dejamos las configuraciones a su estado inicial

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,6 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: true,
-    port: 3000
-  },
-  preview: {
-    host: true,
-    port: 3000
-  },
   plugins: [
     react(),
     tailwindcss(),
